c72ed89b5b9f0d9b597c00af64727ec79a81ea52
SaiVaibhav03 <balusavaibhav907@gmail.com>
2025-04-29 04:48:33 +0530

    fix(server): ensure cookie-parser is used

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-const app = express();
-
+app.use(express.json());
 app.use(
 	cors({
 		origin: 'http://localhost:5173',
 		methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-		// credentials: true,
+		credentials: true,
 	})
 );
-app.use(express.json());
+app.use(cookieParser());
 
 mongoose
 	.connect(process.env.MONGO_URI)
