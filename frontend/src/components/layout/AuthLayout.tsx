59b7484dfdb724451570c242da2ce165e392944b
SaiVaibhav03 <balusavaibhav907@gmail.com>
2025-04-29 15:05:00 +0530

    feat(AuthLayout): add AuthLayout component for authentication pages

---
+++ frontend/src/components/layout/AuthLayout.tsx
@@ -0,0 +1,35 @@
+import NavigationBar from '../NavigationBar.tsx';
+
+type AuthLayoutProps = {
+	title: string;
+	subtitle: string;
+	children: React.ReactNode;
+	linkText: string;
+	linkHref: string;
+};
+
+function AuthLayout({ title, subtitle, children, linkText, linkHref }: AuthLayoutProps) {
+	return (
+		<div className="authLayout">
+			<NavigationBar />
+			<main className="h-10/12 w-screen">
+				<section className="h-full w-full flex flex-row">
+					<aside className="h-full w-3/5"></aside>
+					<section className="h-full w-2/5 flex flex-col justify-center items-center">
+						<h1 className="font-mono text-5xl my-2">{title}</h1>
+						<p className="my-2">{subtitle}</p>
+						{children}
+						<p>
+							{linkText}
+							<a href={linkHref} className="bg-transparent border-0 text-blue-500 pl-1">
+								{linkHref === '/signup' ? 'Sign up' : 'Login'}
+							</a>
+						</p>
+					</section>
+				</section>
+			</main>
+		</div>
+	);
+}
+
+export default AuthLayout;
