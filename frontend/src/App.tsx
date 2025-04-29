f69195eabbc2a418876a73ff3ab02781ce5ad05c
SaiVaibhav03 <balusavaibhav907@gmail.com>
2025-04-29 14:53:17 +0530

    refactor(App): replace Routes with AuthRoutes for improved routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import SignupPage from './pages/SignupPage';
-import LoginPage from './pages/LoginPage';
+import AuthRoutes from './routes/authRoutes.tsx';
 
 function App() {
 	return (
 		<>
-			<Routes>
-				<Route path="/signup" element={<SignupPage />} />
-				<Route path="/login" element={<LoginPage />} />
-			</Routes>
+			<AuthRoutes />
 		</>
 	);
 }
